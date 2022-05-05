5fb8be0694cdc95cd66343f19c61687204299dea
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-05-04 21:01:02 -0300

    :sparkles: feat: Create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransitionNewComponent } from './components/transition-new/transition-new.component';
+import { TransitionListComponent } from './components/transition-list/transition-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'transitions-new', component: TransitionNewComponent },
+  { path: 'transitions', component: TransitionListComponent },
+  { path: '', redirectTo: 'transitions', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
