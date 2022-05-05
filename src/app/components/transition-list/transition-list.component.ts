7a3193adc96d90712fb528352124fcfee5a9b8de
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-05-04 23:11:28 -0300

    :sparkles: feat: Add table transitions

--- src/app/components/transition-list/transition-list.component.ts
+++ src/app/components/transition-list/transition-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transition-list.component.css']
 })
 export class TransitionListComponent implements OnInit {
+  transitions: any;
 
   constructor() { }
 
